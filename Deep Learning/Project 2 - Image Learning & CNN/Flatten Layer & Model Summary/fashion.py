c036c84f0d3f8a3e6d31737702dc7f97b78868d2
eddie <tjdals864237@gmail.com>
2024-06-30 23:26:18 +0900

    gitignore added

--- Deep Learning/Project 2 - Image Learning & CNN/Flatten Layer & Model Summary/fashion.py
+++ Deep Learning/Project 2 - Image Learning & CNN/Flatten Layer & Model Summary/fashion.py
@@ -4,7 +4,22 @@ import tensorflow as tf
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128),
-    tf.keras.layers.Dense(64),
-    tf.keras.layers.Dense(128),
+    tf.keras.layers.Dense(128, input_shape=(28,28), activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10, activation='softmax'),
 ])
+
+#sigmoid: binary prediction -- range 0 - 1, last node 1
+#softmax: category prediction -- range 0 - 1, predicted category.sum = 1
+
+model.summary()
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+model.fit(trainX, trainY, epochs=5)
+
+
+
+
+
+
