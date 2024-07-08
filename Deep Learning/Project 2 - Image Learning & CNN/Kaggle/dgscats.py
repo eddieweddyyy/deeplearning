086654c7d00a2bb1b56ff4fee8dc9728d8ec3136
eddie <tjdals864237@gmail.com>
2024-07-08 23:01:31 +0900

    Test Automation

---
+++ Deep Learning/Project 2 - Image Learning & CNN/Kaggle/dgscats.py
@@ -0,0 +1,73 @@
+# Need kaggle.json & dogs-vscats-redux-kernels-edition/test and train unzipped.
+import os
+
+os.mkdir('/content/dataset')
+os.mkdir('/content/dataset/cats')
+os.mkdir('/content/dataset/dogs')
+
+import tensorflow as tf
+import shutil
+
+print(len(os.listdir('/content/train/')))
+
+for i in os.listdir('/content/train/'):
+  if 'cat' in i:
+    shutil.copyfile('/content/train/' + i, '/content/dataset/cats/' + i)
+  else:
+    shutil.copyfile('/content/train/' + i, '/content/dataset/dogs/' + i)
+
+# tf.keras.preprocessing.image_dataset_from_directory('')
+
+train_ds  = tf.keras.preprocessing.image_dataset_from_directory(
+    '/content/dataset',
+    image_size=(64,64),
+    batch_size=32,
+    subset='training',
+    validation_split=0.2,
+    seed=1234
+)
+
+val_ds  = tf.keras.preprocessing.image_dataset_from_directory(
+    '/content/dataset',
+    image_size=(64,64),
+    batch_size=32,
+    subset='validation',
+    validation_split=0.2,
+    seed=1234
+)
+
+print(train_ds)
+# Data Preprocessing-------------------
+def preprocessing(i, answer):
+  i = tf.cast(i/255.0, tf.float32)
+  return i, answer
+
+train_ds = train_ds.map(preprocessing)
+val_ds = val_ds.map(preprocessing)
+# -------------------------------------
+# import matplotlib.pyplot as plt
+
+for i, answer in train_ds.take(1):
+  print(i)
+  print(answer)
+#   plt.imshow(i[0].numpy().astype('uint8'))
+#   plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=(64, 64, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), padding="same", activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Conv2D(128, (3, 3), padding="same", activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1, activation="sigmoid")
+])
+
+model.summary()
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+model.fit(train_ds, validation_data=val_ds, epochs=5)
---
+++ Deep Learning/Project 2 - Image Learning & CNN/Test Automation/tensorboard.py
@@ -0,0 +1,63 @@
+#Tensorboard and EarlyStopping
+
+import tensorflow as tf
+import numpy as np
+
+(trainX, trainY), (testX, testY) = tf.keras.datasets.fashion_mnist.load_data()
+
+trainX = trainX / 255.0
+testX = testX / 255.0
+
+trainX = trainX.reshape( (trainX.shape[0], 28,28,1) )
+testX = testX.reshape( (testX.shape[0], 28,28,1) )
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax'),
+])
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+import time
+
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir='logs/{}'.format('firstmodel' + str(int(time.time()))))
+
+model.fit(trainX, trainY, validation_data=(testX, testY), epochs=3, callbacks=[tensorboard])
+
+#------------------------------------------------------------------------------------------------------------------
+(trainX, trainY), (testX, testY) = tf.keras.datasets.fashion_mnist.load_data()
+
+trainX = trainX / 255.0
+testX = testX / 255.0
+
+trainX = trainX.reshape( (trainX.shape[0], 28,28,1) )
+testX = testX.reshape( (testX.shape[0], 28,28,1) )
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32,(3,3), padding="same", activation="relu", input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax'),
+])
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir='logs/{}'.format('Conv2model' + str(int(time.time()))))
+
+#EarlyStopping
+es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, mode='min')
+
+
+model.fit(trainX, trainY, validation_data=(testX, testY), epochs=300, callbacks=[tensorboard, es]) #earlystopping
+# -----------------------------------------------------------------------------------------------------------
+def createModel(): #modify so that the function creates model. Use parameters
+  model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32,(3,3), padding="same", activation="relu", input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax'),
+  ])
+  return model
